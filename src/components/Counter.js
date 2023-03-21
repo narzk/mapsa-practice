b54de9bba289ab696a6f70e45d132df9094148eb
Niloofar Arazkhani <n.arazkhani@snapp.express>
2023-03-21 19:49:01 +0330

    fix counting

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,42 +2,30 @@ import { useState } from "react"
 
 export default function Counter() {
   const [count, setCount] = useState(0)
-  const counting=()=>{
+  const [interValId, setInterValId] = useState(null)
 
-//TODO increment
-
-setCount(count=>count+1)
-
-//Previous state. 0->1 , 0+1
-
-  }
-
-  const reset=()=>{
-    setCount(0)
-  }
-
-const handleCounter=(isPaused)=>{
-  const interValId= setInterval(function () {counting()}, 1000);
-  if(isPaused){
-
+  const handleCounter = (isPaused) => {
+    if (!isPaused && !interValId) {
+      setInterValId(setInterval(() => {
+        setCount(count => count + 1)
+      }, 1000))
+    } else if (isPaused) {
       clearInterval(interValId)
+      setInterValId(null)
+    }
   }
-}
 
-
-//reset done
-//increment done
-//decrement 
-//pause
+  const reset = () => {
+    setCount(0)
+    clearInterval(interValId)
+    setInterValId(null)
+  }
 
   return (<>
-  <div>{count}</div>
-  <button onClick={counting}>+</button>
-  <button onClick={reset}>reset</button>
-  <button onClick={()=>handleCounter(false)}>start</button>
-  <button onClick={()=>handleCounter(true)}>pause</button>
-
-
-  </>
-  )
+    <div>{count}</div>
+    <button onClick={() => setCount(count => count + 1)}>+</button>
+    <button onClick={reset}>reset</button>
+    <button onClick={() => handleCounter(false)}>start</button> 
+    <button onClick={() => handleCounter(true)}>pause</button>
+  </>)
 }
